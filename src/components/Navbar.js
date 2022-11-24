5f75b2e5ae4891a81f9ce5011abeea01634698b4
samz9n <samuel.langhoff@outlook.com>
2022-11-24 09:57:52 +0200

    changed logo font and navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,19 +12,18 @@ export default function Navbar(props) {
 
 	return (
 		<Box>
-			<AppBar position="static">
+			<AppBar sx={{backgroundColor:'white'}} position="static">
 				<Toolbar>
 				<Typography className='transformed-text' variant='h6' sx={{marginRight:'20px'}}>MYDISCGOLFPBS</Typography>
 					<Tabs
 						value={props.navTabValue}
 						onChange={handleChange}
-						textColor="inherit"
+						textColor="primary"
 						indicatorColor="secondary"
-						aria-label="secondary tabs example"
 					>
-						<Tab value="one" label="Home" component={Link} to='home' />
-						<Tab value="two" label="Add Score" component={Link} to='addscore' />
-						<Tab value="three" label="My Best Rounds" component={Link} to='mybestrounds' />
+						<Tab sx={{marginLeft:'20px'}} value="one" label="Home" component={Link} to='home' />
+						<Tab sx={{marginLeft:'20px'}} value="two" label="Add Score" component={Link} to='addscore' />
+						<Tab sx={{marginLeft:'20px'}} value="three" label="My Best Rounds" component={Link} to='mybestrounds' />
 					</Tabs>
 				</Toolbar>
 			</AppBar>
