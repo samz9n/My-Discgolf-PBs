4d5ea2b2a2a8b620ca6580374c2d8694fa70cfe0
samz9n <samuel.langhoff@outlook.com>
2022-12-09 12:40:15 +0200

    cleaned comments from backend

--- backend/myServer.js
+++ backend/myServer.js
@@ -79,6 +79,7 @@ app.get('/round/delete/:id', (req, res, next) => {
 	});
 });
 
+//EDIT ONE ROUND BY ID
 app.put('/round/edit/:id', (req, res, next) => {
 	let data = {
 		score: req.body.score,
@@ -121,76 +122,6 @@ app.put('/round/edit/:id', (req, res, next) => {
 		}
 	);
 });
-
-/* app.patch('/round/edit/:id', (req, res, next) => {
-	let data = {
-		score: req.body.score,
-		holeinones: req.body.holeinones,
-		birdies: req.body.birdies,
-		pars: req.body.pars,
-		bogeys: req.body.bogeys,
-		doublebogeys: req.body.doublebogeys,
-		tripleorworse: req.body.tripleorworse
-	};
-	let id = req.params.id;
-	db.run(
-		`UPDATE bestRound set 
-           score = COALESCE(?,score), 
-           holeinones = COALESCE(?,holeinones),
-		   birdies = COALESCE(?,birdies),
-		   pars = COALESCE(?,pars),
-		   bogeys = COALESCE(?,bogeys),
-		   doublebogeys = COALESCE(?,doublebogeys),
-		   tripleorworse = COALESCE(?,tripleorworse)
-           WHERE id = ?`,
-		[ data.score, data.holeinones, data.birdies, data.pars, data.bogeys, data.doublebogeys, data.tripleorworse, id],
-		function(err, result) {
-			if (err) {
-				return res.status(400).json({ error: res.message, data: data});
-				
-			}
-				return res.status(200).json({
-				data: data,
-				changes: this.changes
-			});
-		}
-	);
-}); */
-
-//FOR IMAGES(POST)
-/* const multer = require('multer');
-
-const storage = multer.diskStorage({
-	destination: (req, file, callback) => {
-		callback(null, './images'); // Mihin kansioon ladataan
-	},
-	filename: (req, file, callback) => {
-		callback(null, file.originalname);  // Millä tiedostonimellä
-	}
-});
-
-const upload = multer({ storage: storage });
-
-app.post('/round/add', upload.single('kuva'), (req, res, next) => {
-	let round = req.body;
-
-	let kuvaNimi = null;
-	if (req.file) {
-		kuvaNimi = req.file.originalname;
-	}
-
-	db.run('insert into round (otsikko,paiva,paikka,saa,kuva,kuvaus) values (?, ?, ?, ?, ?, ?)',
-		[round.otsikko, round.paiva, round.paikka, round.saa, kuvaNimi, round.kuvaus], (error, result) => {
-			if (error) throw error;
-
-			return res.status(200).json({ count: 1 });
-		});
-})
- */
-/* app.get('/download/:nimi', (req, res, next) => {
-	var file = './images/' + req.params.nimi;
-	res.download(file);
-}); */
 
 app.get('*', (req, res, next) => {
 	return res.status(404).json({ error: true, message: 'Nothing to fetch' });
